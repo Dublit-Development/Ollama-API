d4be6bb4c1163a2150d1d4ad34b54ae8c3753d18
Joseph <joemenjr@gmail.com>
2024-04-09 00:05:15 -0400

    litellm class

--- app/libs/litellm.py
+++ app/libs/litellm.py
@@ -1,12 +1,20 @@
 from litellm import completion
-import json
 
-def llm_query(applicantPrompt, allyPrompt, santandarPrompt, wellFargo, capitalOne, questionPrompt):
-    response = completion(
-                model="ollama/gemma:7b", 
-                messages = [{ "content": f"""  The applicant has provided the following information {applicantPrompt}. {questionPrompt}.
-                {allyPrompt}, {santandarPrompt}, {wellFargo}, {capitalOne}.""","role": "user"}], 
-                api_base="http://localhost:11434"
-    )
+class litellmContruct:
+    def __init__(self, model="ollama/llama2", api_base="http://localhost:11434", message="Why is the sky Blue?"):
+        self.model = model
+        self.api_base = api_base
+        self.message = message
 
-    return response
+    def model_completion(self):
+        
+        response = completion(
+            model="ollama/llama2",
+            messages = [{ "content": self.message,"role": "user"}],
+            api_base=self.api_base
+        )
+
+        return response
+
+response = litellmContruct()
+print(response.model_completion())
